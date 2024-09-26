37bdabc17c428bc814b90761488f32ca92403afa
tianmaoyu <tianmaoyu@link-smart.cn>
2024-09-26 14:46:29 +0800

    参数说明

--- models/yolo.py
+++ models/yolo.py
@@ -33,6 +33,7 @@ class Detect(nn.Module):
         self.na = len(anchors[0]) // 2  # number of anchors
         self.grid = [torch.zeros(1)] * self.nl  # init grid
         a = torch.tensor(anchors).float().view(self.nl, -1, 2)
+        # 正向传播更新，反向不需要更新 buffer
         self.register_buffer('anchors', a)  # shape(nl,na,2)
         self.register_buffer('anchor_grid', a.clone().view(self.nl, 1, -1, 1, 1, 2))  # shape(nl,1,na,1,1,2)
         self.m = nn.ModuleList(nn.Conv2d(x, self.no * self.na, 1) for x in ch)  # output conv
